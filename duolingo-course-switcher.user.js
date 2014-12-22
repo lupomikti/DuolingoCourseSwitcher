ae31613c07932b866d29cd97caa5806e200dab26
Arek Olek <arkadiusz.olek@uj.edu.pl>
2014-12-22 22:20:05 +0100

    bump version

--- duolingo-course-switcher.user.js
+++ duolingo-course-switcher.user.js
@@ -4,7 +4,7 @@
 // @include     https://www.duolingo.com/*
 // @downloadURL https://github.com/arekolek/DuolingoCourseSwitcher/raw/master/duolingo-course-switcher.user.js
 // @updateURL   https://github.com/arekolek/DuolingoCourseSwitcher/raw/master/duolingo-course-switcher.user.js
-// @version     0.6.0
+// @version     0.6.1
 // @grant       none
 // ==/UserScript==
 
